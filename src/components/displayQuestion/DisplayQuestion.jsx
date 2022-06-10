8de06a7a9fd856ae05b99087261ce614107582af
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-09 21:51:25 -0300

    add edit display flex - space around displayQuestion

--- src/components/displayQuestion/DisplayQuestion.jsx
+++ src/components/displayQuestion/DisplayQuestion.jsx
@@ -1,11 +1,17 @@
 import './displayQuestion.css'
 
+function handleInput(event) {
+  if (event.key === 'Enter') {
+    console.log(event.target.value)
+  }
+}
+
 export default function DisplayQuestion() {
   return (
     <div className="main-container">
       <div className='display-box'>
-        <div>Question</div>
-        <input type="text" />
+        <div className='question-text'>Question</div>
+        <input className='input-answer' type="text" onKeyUp={handleInput} />
       </div>
     </div>
   )
