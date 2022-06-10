fb091c98bdc473a3655318caa3ad87f5fecd2b8e
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-10 20:32:22 -0300

    update audio list

---
+++ src/audios/numberAudios/numberAudiosArray.js
@@ -0,0 +1,25 @@
+import zero from './0.mp3';
+import one from './1.mp3';
+import two from './2.mp3';
+import three from './3.mp3';
+import four from './4.mp3';
+import five from './5.mp3';
+import six from './6.mp3';
+import seven from './7.mp3';
+import eight from './8.mp3';
+import nine from './9.mp3';
+import ten from './10.mp3';
+
+export const numberAudiosArray = [
+  { name: '0', audio: zero },
+  { name: '1', audio: one },
+  { name: '2', audio: two },
+  { name: '3', audio: three },
+  { name: '4', audio: four },
+  { name: '5', audio: five },
+  { name: '6', audio: six },
+  { name: '7', audio: seven },
+  { name: '8', audio: eight },
+  { name: '9', audio: nine },
+  { name: '10', audio: ten },
+]
--- src/components/displayQuestion/DisplayQuestion.jsx
+++ src/components/displayQuestion/DisplayQuestion.jsx
@@ -1,17 +1,24 @@
+import { useState } from 'react'
 import './displayQuestion.css'
-
-function handleInput(event) {
-  if (event.key === 'Enter') {
-    console.log(event.target.value)
-  }
-}
+import { numberAudiosArray } from '../../audios/numberAudios/numberAudiosArray.js'
 
 export default function DisplayQuestion() {
+  const [arrayItem, setArrayItem] = useState(0)
+  const myAudio = new Audio(numberAudiosArray[arrayItem].audio)
+
+  function handleInput(event) {
+    if (event.key === 'Enter') {
+      console.log(event.target.value)
+      setArrayItem(parseInt(event.target.value))
+    }
+  }
+
   return (
     <div className="main-container">
       <div className='display-box'>
         <div className='question-text'>Question</div>
         <input className='input-answer' type="text" onKeyUp={handleInput} />
+        <button onClick={() => myAudio.play()}>Play</button>
       </div>
     </div>
   )
