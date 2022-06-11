997de2dce75267873f1991bef2eed35a5c54a4ca
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-10 21:41:17 -0300

    random listening update on DisplayQuestion

--- src/components/displayQuestion/DisplayQuestion.jsx
+++ src/components/displayQuestion/DisplayQuestion.jsx
@@ -1,16 +1,42 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import './displayQuestion.css'
 import { numberAudiosArray } from '../../audios/numberAudios/numberAudiosArray.js'
 
 export default function DisplayQuestion() {
-  const [arrayItem, setArrayItem] = useState(0)
+  const [arrayItem, setArrayItem] = useState(10)
+  const [isStarting, setIsStarting] = useState(false)
   const myAudio = new Audio(numberAudiosArray[arrayItem].audio)
 
-  function handleInput(event) {
-    if (event.key === 'Enter') {
-      console.log(event.target.value)
-      setArrayItem(parseInt(event.target.value))
+  useEffect(() => {
+    if (isStarting) {
+      myAudio.play()
     }
+  }, [arrayItem])
+
+  // console.log(arrayItem, numberAudiosArray[arrayItem])
+
+  function getRandomIntInclusive(min, max) {
+    min = Math.ceil(min);
+    max = Math.floor(max);
+    return Math.floor(Math.random() * (max - min + 1)) + min;
+  }
+
+  function handleInput(event) {
+    console.log('eventKey', event.key)
+    if (event.key === 'Enter') {
+      if (numberAudiosArray[arrayItem].name === event.target.value) {
+        console.log('right')
+      } else {
+        console.log('wrong')
+      }
+      setArrayItem(prev => getRandomIntInclusive(0, numberAudiosArray.length - 1))  //0 is the min number
+      event.target.value = '';
+    }
+  }
+
+  function startQuestion() {
+    myAudio.play()
+    setIsStarting(true)
   }
 
   return (
@@ -18,7 +44,7 @@ export default function DisplayQuestion() {
       <div className='display-box'>
         <div className='question-text'>Question</div>
         <input className='input-answer' type="text" onKeyUp={handleInput} />
-        <button onClick={() => myAudio.play()}>Play</button>
+        <button onClick={() => startQuestion()}>Start</button>
       </div>
     </div>
   )
