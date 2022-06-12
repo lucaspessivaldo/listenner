44773dd6b23a3600ac516f3a34a3c62285bfe725
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-11 21:27:36 -0300

    add audio number 11 to 100

--- src/audios/numberAudios/numberAudiosArray.js
+++ src/audios/numberAudios/numberAudiosArray.js
@@ -9,6 +9,96 @@ import seven from './7.mp3';
 import eight from './8.mp3';
 import nine from './9.mp3';
 import ten from './10.mp3';
+import eleven from './11.mp3';
+import twelve from './12.mp3';
+import thirteen from './13.mp3';
+import fourteen from './14.mp3';
+import fifteen from './15.mp3';
+import sixteen from './16.mp3';
+import seventeen from './17.mp3';
+import eighteen from './18.mp3';
+import nineteen from './19.mp3';
+import twenty from './20.mp3';
+import twenty_one from './21.mp3';
+import twenty_two from './22.mp3';
+import twenty_three from './23.mp3';
+import twenty_four from './24.mp3';
+import twenty_five from './25.mp3';
+import twenty_six from './26.mp3';
+import twenty_seven from './27.mp3';
+import twenty_eight from './28.mp3';
+import twenty_nine from './29.mp3';
+import thirty from './30.mp3';
+import thirty_one from './31.mp3';
+import thirty_two from './32.mp3';
+import thirty_three from './33.mp3';
+import thirty_four from './34.mp3';
+import thirty_five from './35.mp3';
+import thirty_six from './36.mp3';
+import thirty_seven from './37.mp3';
+import thirty_eight from './38.mp3';
+import thirty_nine from './39.mp3';
+import forty from './40.mp3';
+import forty_one from './41.mp3';
+import forty_two from './42.mp3';
+import forty_three from './43.mp3';
+import forty_four from './44.mp3';
+import forty_five from './45.mp3';
+import forty_six from './46.mp3';
+import forty_seven from './47.mp3';
+import forty_eight from './48.mp3';
+import forty_nine from './49.mp3';
+import fifty from './50.mp3';
+import fifty_one from './51.mp3';
+import fifty_two from './52.mp3';
+import fifty_three from './53.mp3';
+import fifty_four from './54.mp3';
+import fifty_five from './55.mp3';
+import fifty_six from './56.mp3';
+import fifty_seven from './57.mp3';
+import fifty_eight from './58.mp3';
+import fifty_nine from './59.mp3';
+import sixty from './60.mp3';
+import sixty_one from './61.mp3';
+import sixty_two from './62.mp3';
+import sixty_three from './63.mp3';
+import sixty_four from './64.mp3';
+import sixty_five from './65.mp3';
+import sixty_six from './66.mp3';
+import sixty_seven from './67.mp3';
+import sixty_eight from './68.mp3';
+import sixty_nine from './69.mp3';
+import seventy from './70.mp3';
+import seventy_one from './71.mp3';
+import seventy_two from './72.mp3';
+import seventy_three from './73.mp3';
+import seventy_four from './74.mp3';
+import seventy_five from './75.mp3';
+import seventy_six from './76.mp3';
+import seventy_seven from './77.mp3';
+import seventy_eight from './78.mp3';
+import seventy_nine from './79.mp3';
+import eighty from './80.mp3';
+import eighty_one from './81.mp3';
+import eighty_two from './82.mp3';
+import eighty_three from './83.mp3';
+import eighty_four from './84.mp3';
+import eighty_five from './85.mp3';
+import eighty_six from './86.mp3';
+import eighty_seven from './87.mp3';
+import eighty_eight from './88.mp3';
+import eighty_nine from './89.mp3';
+import ninety from './90.mp3';
+import ninety_one from './91.mp3';
+import ninety_two from './92.mp3';
+import ninety_three from './93.mp3';
+import ninety_four from './94.mp3';
+import ninety_five from './95.mp3';
+import ninety_six from './96.mp3';
+import ninety_seven from './97.mp3';
+import ninety_eight from './98.mp3';
+import ninety_nine from './99.mp3';
+import one_hundred from './100.mp3';
 
 export const numberAudiosArray = [
   { name: '0', audio: zero },
@@ -22,4 +112,94 @@ export const numberAudiosArray = [
   { name: '8', audio: eight },
   { name: '9', audio: nine },
   { name: '10', audio: ten },
-]
+
+  { name: '11', audio: eleven },
+  { name: '12', audio: twelve },
+  { name: '13', audio: thirteen },
+  { name: '14', audio: fourteen },
+  { name: '15', audio: fifteen },
+  { name: '16', audio: sixteen },
+  { name: '17', audio: seventeen },
+  { name: '18', audio: eighteen },
+  { name: '19', audio: nineteen },
+  { name: '20', audio: twenty },
+  { name: '21', audio: twenty_one },
+  { name: '22', audio: twenty_two },
+  { name: '23', audio: twenty_three },
+  { name: '24', audio: twenty_four },
+  { name: '25', audio: twenty_five },
+  { name: '26', audio: twenty_six },
+  { name: '27', audio: twenty_seven },
+  { name: '28', audio: twenty_eight },
+  { name: '29', audio: twenty_nine },
+  { name: '30', audio: thirty },
+  { name: '31', audio: thirty_one },
+  { name: '32', audio: thirty_two },
+  { name: '33', audio: thirty_three },
+  { name: '34', audio: thirty_four },
+  { name: '35', audio: thirty_five },
+  { name: '36', audio: thirty_six },
+  { name: '37', audio: thirty_seven },
+  { name: '38', audio: thirty_eight },
+  { name: '39', audio: thirty_nine },
+  { name: '40', audio: forty },
+  { name: '41', audio: forty_one },
+  { name: '42', audio: forty_two },
+  { name: '43', audio: forty_three },
+  { name: '44', audio: forty_four },
+  { name: '45', audio: forty_five },
+  { name: '46', audio: forty_six },
+  { name: '47', audio: forty_seven },
+  { name: '48', audio: forty_eight },
+  { name: '49', audio: forty_nine },
+  { name: '50', audio: fifty },
+  { name: '51', audio: fifty_one },
+  { name: '52', audio: fifty_two },
+  { name: '53', audio: fifty_three },
+  { name: '54', audio: fifty_four },
+  { name: '55', audio: fifty_five },
+  { name: '56', audio: fifty_six },
+  { name: '57', audio: fifty_seven },
+  { name: '58', audio: fifty_eight },
+  { name: '59', audio: fifty_nine },
+  { name: '60', audio: sixty },
+  { name: '61', audio: sixty_one },
+  { name: '62', audio: sixty_two },
+  { name: '63', audio: sixty_three },
+  { name: '64', audio: sixty_four },
+  { name: '65', audio: sixty_five },
+  { name: '66', audio: sixty_six },
+  { name: '67', audio: sixty_seven },
+  { name: '68', audio: sixty_eight },
+  { name: '69', audio: sixty_nine },
+  { name: '70', audio: seventy },
+  { name: '71', audio: seventy_one },
+  { name: '72', audio: seventy_two },
+  { name: '73', audio: seventy_three },
+  { name: '74', audio: seventy_four },
+  { name: '75', audio: seventy_five },
+  { name: '76', audio: seventy_six },
+  { name: '77', audio: seventy_seven },
+  { name: '78', audio: seventy_eight },
+  { name: '79', audio: seventy_nine },
+  { name: '80', audio: eighty },
+  { name: '81', audio: eighty_one },
+  { name: '82', audio: eighty_two },
+  { name: '83', audio: eighty_three },
+  { name: '84', audio: eighty_four },
+  { name: '85', audio: eighty_five },
+  { name: '86', audio: eighty_six },
+  { name: '87', audio: eighty_seven },
+  { name: '88', audio: eighty_eight },
+  { name: '89', audio: eighty_nine },
+  { name: '90', audio: ninety },
+  { name: '91', audio: ninety_one },
+  { name: '92', audio: ninety_two },
+  { name: '93', audio: ninety_three },
+  { name: '94', audio: ninety_four },
+  { name: '95', audio: ninety_five },
+  { name: '96', audio: ninety_six },
+  { name: '97', audio: ninety_seven },
+  { name: '98', audio: ninety_eight },
+  { name: '99', audio: ninety_nine },
+  { name: '100', audio: one_hundred }]
