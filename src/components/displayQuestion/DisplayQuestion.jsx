2815afa2f9cb8596e51c7bd0a75a0c6fea231b16
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-06-11 22:15:47 -0300

    Update - Check answer while is typing

--- src/components/displayQuestion/DisplayQuestion.jsx
+++ src/components/displayQuestion/DisplayQuestion.jsx
@@ -6,6 +6,7 @@ export default function DisplayQuestion() {
   const [arrayItem, setArrayItem] = useState(10)
   const [isStarting, setIsStarting] = useState(false)
   const [isNumberArrayChanged, setIsNumberArrayChanged] = useState(false)
+  const [isFocus, setIsFocus] = useState(false)
   const currentAudio = new Audio(numberAudiosArray[arrayItem].audio)
 
   useEffect(() => {
@@ -21,17 +22,19 @@ export default function DisplayQuestion() {
   }
 
   function handleInput(event) {
-    console.log('eventKey', event.key)
-    if (event.key === 'Enter') {
-      if (numberAudiosArray[arrayItem].name === event.target.value) {
-        console.log('right')
-      } else {
-        console.log('wrong')
-      }
+    if (numberAudiosArray[arrayItem].name === event.target.value) {
       setArrayItem(getRandomIntInclusive(0, numberAudiosArray.length - 1))  //0 is the min number
       setIsNumberArrayChanged(!isNumberArrayChanged)
       event.target.value = '';
     }
+  }
+
+  function onFocusHandler() {
+    setIsFocus(true)
+  }
+
+  function onBlurHandler() {
+    setIsFocus(false)
   }
 
   function startQuestion() {
@@ -42,7 +45,14 @@ export default function DisplayQuestion() {
     <div className="main-container">
       <div className='display-box'>
         <div className='question-text'>Question</div>
-        <input className='input-answer' type="text" onKeyUp={handleInput} />
+        <input
+          className='input-answer'
+          type="text"
+          onChange={handleInput}
+          placeholder={isFocus ? '' : 'Type your answer here'}
+          onFocus={onFocusHandler}
+          onBlur={onBlurHandler}
+        />
         <button onClick={() => startQuestion()}>Start</button>
       </div>
     </div>
