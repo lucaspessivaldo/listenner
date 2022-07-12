18f32eccbb73516fba1433928372a8e84945d0b8
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-07-11 22:18:27 -0300

    add - minmalPairs audios

---
+++ src/audios/minimalPairsAudios/minimalPairsArray.js
@@ -0,0 +1,48 @@
+import seat from './001-seat.mp3';
+import sit from './001-sit.mp3';
+import feet from './002-feet.mp3';
+import fit from './002-fit.mp3';
+import heat from './003-heat.mp3';
+import hit from './003-hit.mp3';
+import cheap from './004-cheap.mp3';
+import chip from './004-chip.mp3';
+import steal from './005-steal.mp3';
+import still from './005-still.mp3';
+import knit from './006-knit.mp3';
+import neat from './006-neat.mp3';
+import seek from './007-seek.mp3';
+import sick from './007-sick.mp3';
+import eat from './008-eat.mp3';
+import it from './008-it.mp3';
+import bean from './009-bean.mp3';
+import bin from './009-bin.mp3';
+
+export const alphabetAudiosArray = [
+  { word: 'seat', audio: seat, phonetic: 'sit' },
+  { word: 'sit', audio: sit, phonetic: 'sit' },
+
+  { word: 'feet', audio: feet, phonetic: 'fit' },
+  { word: 'fit', audio: fit, phonetic: 'fɪt' },
+
+  { word: 'heat', audio: heat, phonetic: 'hit' },
+  { word: 'hit', audio: hit, phonetic: 'hɪt' },
+
+  { word: 'cheap', audio: cheap, phonetic: 'ʧip' },
+  { word: 'chip', audio: chip, phonetic: 'ʧɪp' },
+
+  { word: 'steal', audio: steal, phonetic: 'stil' },
+  { word: 'still', audio: still, phonetic: 'stɪl' },
+
+  { word: 'knit', audio: knit, phonetic: 'nɪt' },
+  { word: 'neat', audio: neat, phonetic: 'nit' },
+
+  { word: 'seek', audio: seek, phonetic: 'sik' },
+  { word: 'sick', audio: sick, phonetic: 'sɪk' },
+
+  { word: 'eat', audio: eat, phonetic: 'it' },
+  { word: 'it', audio: it, phonetic: 'ɪt' },
+
+  { word: 'bean', audio: bean, phonetic: 'bin' },
+  { word: 'bin', audio: bin, phonetic: 'bɪn' },
+
+]
