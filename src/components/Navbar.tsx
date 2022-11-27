2d89ebb3c1dc9d8f60e6d8b6501f819fd5064b0c
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-11-27 15:50:10 -0300

    change website name

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -1,26 +1,31 @@
-import { Link } from 'react-router-dom'
-import logo from '../assets/images/logo.png'
+import { useEffect, useState } from 'react'
+import { Link, useLocation } from 'react-router-dom'
 
 export default function Navbar() {
+  const [isHomepage, setIsHomepage] = useState(true)
+  const location = useLocation()
+
+  useEffect(() => {
+    if (location.pathname === '/') setIsHomepage(true)
+    else setIsHomepage(false)
+  }, [location])
+
   return (
     <header className='border-b border-black'>
-      <nav className="cointainer max-w-7xl mx-auto flex items-center justify-between px-3">
+      <nav className="cointainer h-12 max-w-7xl mx-auto flex items-center justify-between px-3">
 
         <Link to='/'>
           <div className='flex items-center gap-1 cursor-pointer select-none'>
-            <img className='w-6 h-6' src={logo} alt="logo" />
-            <div className='font-inter font-semibold  text-2xl'>English</div>
+            <div className='font-inter font-semibold  text-2xl'>listenner.</div>
           </div>
         </Link>
 
-        <div className='flex gap-2'>
-          <button className="font-inter font-semibold text-sm  px-4 hover:underline">
-            Login
+        <Link to='/learn' className={`${isHomepage ? 'block' : 'hidden'}`}>
+          <button className="border-2 border-black font-inter font-semibold text-sm text-black px-3 py-1 my-2 rounded-md hover:bg-black hover:text-white">
+            Get Started
           </button>
-          <button className="border-2 border-black font-inter font-semibold text-sm text-black px-4 py-1 my-2 rounded-md hover:bg-black hover:text-white">
-            Sign up
-          </button>
-        </div>
+        </Link>
+
       </nav>
     </header>
   )
