35e487e1ce9e05cb4224f9256f312cb240eb41b6
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-11-27 23:25:50 -0300

    fix - feature transparent font

--- src/components/homepage/FeatureItem.tsx
+++ src/components/homepage/FeatureItem.tsx
@@ -10,7 +10,7 @@ export default function FeatureItem(props: FeatureItemProp) {
     <div className="flex flex-col lg:flex-row">
       <PolygonSvg />
       <div>
-        <h6 className="text-transparent bg-clip-text bg-gradient-to-r text-black mb-2 font-bold leading-5">{props.title}</h6>
+        <h6 className="text-black mb-2 font-bold">{props.title}</h6>
         <p className="mb-3 text-sm font-inter text-black">{props.text}</p>
       </div>
     </div>
