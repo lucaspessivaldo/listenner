d6fe368016758bb0982b54c61f621e6788ba5a29
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-12-06 21:55:42 -0300

    Update learn page

--- src/components/learn/QuestionButton.tsx
+++ src/components/learn/QuestionButton.tsx
@@ -1,8 +1,19 @@
+interface Props {
+  text: string
+  audio: HTMLAudioElement
+  setSelectedButton: (selectedButton: string) => void
+  isSelected: boolean
+}
 
-export default function QuestionButton() {
+const buttonStyle = "border-[1px] font-inter font-semibold sm:w-48 w-64 h-11 mx-2 my-2 transition-shadow rounded-lg"
+
+export default function QuestionButton({ text, audio, setSelectedButton, isSelected }: Props) {
   return (
-    <button className="border-2 border-black font-inter font-semibold sm:w-48 w-64 h-11 mx-2 my-2 hover:bg-stone-300/80 rounded-lg">
-      text
+    <button
+      className={`${buttonStyle} ${isSelected ? 'border-black shadow-[3px_3px_0px_rgb(0,0,0)]' : 'border-zinc-600 text-zinc-600'}`}
+      onClick={() => setSelectedButton(text)}
+    >
+      {text}
     </button>
   )
 }
--- src/pages/Learn.tsx
+++ src/pages/Learn.tsx
@@ -1,29 +1,43 @@
 import QuestionButton from '../components/learn/QuestionButton'
 import SelectCategory from '../components/learn/SelectCategory'
 import SpeakerSvg from '../components/learn/SpeakerSvg'
-
+import alphabetAudiosArray from '../assets/audios/alphabet/alphabetAudios'
 import { AudioArray } from '../common/types'
 
-import alphabetAudiosArray from '../assets/audios/alphabet/alphabetAudios'
+import { useEffect, useState } from 'react'
 
-import type { RootState } from '../app/store'
-import { useSelector, useDispatch } from 'react-redux'
+
+function getRandomIntInclusive(max: number) {
+  const min = 0
+  max = Math.floor(max);
+  return Math.floor(Math.random() * (max - min + 1) + min);
+}
+
+function getRandomPosition(array: AudioArray[]) {
+  const randomNumber = getRandomIntInclusive(array.length - 1)
+  const randomWord = array[randomNumber]
+  return randomWord
+}
 
 export default function Learn() {
-  const selectedTopic = useSelector((state: RootState) => state.selectedTopic.value)
-  const dispatch = useDispatch()
+  const [currentWords, setCurrentWords] = useState<AudioArray[]>([alphabetAudiosArray[0]])
+  const [rightAnswer, setRightAnswer] = useState<AudioArray>(alphabetAudiosArray[0])
+  const [selectedButton, setSelectedButton] = useState<String>('')
 
-  function getRandomIntInclusive(max: number) {
-    const min = 0
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
-  }
+  useEffect(() => {
+    let firstWord = getRandomPosition(alphabetAudiosArray)
+    let secondWord = getRandomPosition(alphabetAudiosArray)
 
-  function getRandomPosition(array: AudioArray[]) {
-    const randomNumber = getRandomIntInclusive(array.length - 1)
-    const randomWord = array[randomNumber]
-    return randomWord
-  }
+    while (secondWord.text === firstWord.text) {
+      secondWord = getRandomPosition(alphabetAudiosArray)
+    }
+
+    const array = [firstWord, secondWord]
+    array.sort(() => Math.random() - 0.5)
+
+    setRightAnswer(firstWord)
+    setCurrentWords(array)
+  }, [])
 
   return (
     <div className="container mx-auto max-w-4xl flex flex-col items-center w-full mt-6 px-3 rounded-md h-5/6 ">
@@ -32,28 +46,36 @@ export default function Learn() {
 
       <div
         onClick={() => {
-          const currentAudio = alphabetAudiosArray[2].audio
-          currentAudio.pause()
-          currentAudio.play()
+          rightAnswer.audio.pause()
+          rightAnswer.audio.play()
+
         }}
         className='cursor-pointer border-2 border-black w-40 h-40 flex items-center justify-center rounded-3xl 
-        shadow-[5px_5px_0px_rgb(0,0,0)] active:shadow-[1px_1px_0px_rgb(0,0,0)] text-black bg-white 
-        ease-out active:translate-y-1 my-10 transition-all hover:bg-stone-300/80'
+        shadow-[5px_5px_0px_rgb(0,0,0)] active:shadow-[1px_1px_0px_rgb(0,0,0)] text-black
+        ease-out active:translate-y-1 my-10 transition-all'
       >
         <SpeakerSvg />
       </div>
 
+      <div className='text-zinc-600 font-inter'>
+        select the right answer
+      </div>
+
       <div className='mb-7 grid grid-cols-1 sm:grid-cols-2'>
-        <QuestionButton />
-        <QuestionButton />
-        <QuestionButton />
-        <QuestionButton />
+        {currentWords.map(word => (
+          <QuestionButton
+            setSelectedButton={setSelectedButton}
+            isSelected={word.text === selectedButton}
+            audio={word.audio}
+            text={word.text}
+            key={word.text}
+          />
+        ))}
       </div>
 
       <button className='bg-black text-white font-inter font-semibold  w-64 h-11 rounded-lg hover:bg-zinc-900'>
         check the answer
       </button>
-
     </div>
   )
 }
