74f0d1c5033eed798ed30412b9cce2e4add19901
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2022-12-31 20:07:43 -0300

    update - put tests on tests folder

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,7 +1,7 @@
-import { render, fireEvent } from '@testing-library/react'
+import { render } from '@testing-library/react'
 import { describe, it } from 'vitest'
 
-import Footer from '../src/components/Footer'
+import Footer from '../components/Footer'
 
 describe('App', () => {
   it('renders the Footer component', () => {
