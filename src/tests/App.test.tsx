38357079698e7142886ef91d3a7d991416886151
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2023-01-02 23:22:06 -0300

    update - create more tests to main page

--- src/tests/App.test.tsx
+++ src/tests/App.test.tsx
@@ -1,11 +1,36 @@
-import { render } from '@testing-library/react'
+import { render, screen } from '@testing-library/react'
 import { describe, it } from 'vitest'
 
+import {
+  RouterProvider,
+  createMemoryRouter
+} from "react-router-dom";
+
 import Footer from '../components/Footer'
+import Navbar from '../components/Navbar'
 
 describe('App', () => {
   it('renders the Footer component', () => {
-    const { getByText } = render(<Footer />)
-    expect(getByText('Vercel')).toBeInTheDocument();
+    render(<Footer />)
+    const footerText = screen.getByText(/The source code is available on/i)
+    expect(footerText).toBeInTheDocument()
   })
+})
+
+
+test('Navbar displays "Get Started" button on homepage', async () => {
+  const routesConfig = [
+    {
+      path: "/",
+      element: <Navbar />,
+    },
+  ]
+
+  const currentPage = createMemoryRouter(routesConfig, {
+    initialEntries: ["/"]
+  })
+
+  render(<RouterProvider router={currentPage} />);
+  const footerText = screen.getByText(/Get Started/i)
+  expect(footerText).toBeInTheDocument()
 })
