1ef9488476b3e9c173444c1bf45334f7ff9dd306
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2023-01-02 23:23:09 -0300

    update - add button color when right or wrong answer

--- src/components/learn/QuestionButton.tsx
+++ src/components/learn/QuestionButton.tsx
@@ -9,7 +9,7 @@ interface Props {
 
 const buttonStyle = "border-[1px] font-inter font-semibold sm:w-48 w-64 h-11 mx-2 my-2 transition-shadow rounded-lg"
 const selectedAndRight = 'border-green-600 shadow-[0px_2px_0px_rgb(18,145,65)]'
-const selectedAndWrong = 'border-red-600 shadow-[0px_2px_0px_rgb(220,38,38)]'
+const selectedAndWrong = 'border-red-600 sha shadow-[0px_2px_0px_rgb(220,38,38)]'
 const unselected = 'border-zinc-600 text-zinc-600'
 
 export default function QuestionButton({ text, setSelectedButton, isSelected, isAnswerd, rightAnser }: Props) {
