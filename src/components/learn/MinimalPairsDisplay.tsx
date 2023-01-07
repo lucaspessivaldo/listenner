d8e3f6a88143791d265f0f55db1312fc04961940
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2023-01-07 18:46:53 -0300

    update - Minimal Pair page play audio when refresh page

--- src/components/learn/MinimalPairsDisplay.tsx
+++ src/components/learn/MinimalPairsDisplay.tsx
@@ -56,6 +56,10 @@ export default function MinimalPairsDisplay() {
   }
 
   useEffect(() => {
+    restartQuestion()
+  }, [])
+
+  useEffect(() => {
     if (selectedButton === '') return
     checkAnswer()
   }, [selectedButton])
