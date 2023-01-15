d947c38673d82df94aeeb575129816639f91ccce
LucasPessivaldo <11798065+lucaspessivaldo@users.noreply.github.com>
2023-01-15 20:20:52 -0300

    fix - remove dark bg setting button

--- src/components/SettingButton.tsx
+++ src/components/SettingButton.tsx
@@ -37,7 +37,7 @@ export default function SettingButton(_props: Props) {
             className={
               `border-2 border-black radix-side-top:animate-slide-up radix-side-bottom:animate-slide-down
               w-48 rounded-lg px-1.5 py-1 shadow-md md:w-56
-              bg-white dark:bg-gray-800`
+              bg-white`
             }
           >
 
